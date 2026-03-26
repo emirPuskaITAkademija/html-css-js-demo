6ab1de23bbdc385e8b1983f435e716b179665348
emirPuskaITAkademija <emir.puska@it-akademija.com>
2026-03-26 20:02:50 +0100

    Pagination, Sorting, Filtering

--- 6/table/example6.js
+++ 6/table/example6.js
@@ -16,8 +16,46 @@ let igraci = [
     {ime: "Selma", bodovi: 95}
 ];
 
+let filterIme = "";
+let filterBodovi = "";
+let sortKolona = null;
+let sortSmjer = "asc";
+let pageSize = 5;
+let currentPage = 1;
+
 function prikaziIgrace() {
     const tableHtmlElement = document.getElementById("tabla");
+
+    let rezultat = [...igraci];
+
+    if(filterIme !== ""){
+        rezultat = rezultat.filter(igrac => igrac.ime.toLowerCase().includes(filterIme.toLowerCase()));
+    }
+    if(filterBodovi !== ""){
+        rezultat = rezultat.filter(igrac => igrac.bodovi>=Number(filterBodovi));
+    }
+    if(sortKolona === "ime"){
+        rezultat.sort((a, b) => sortSmjer==="asc" ?
+            a.ime.localeCompare(b.ime) : b.ime.localeCompare(a.ime));
+    }
+    if(sortKolona === "bodovi"){
+        rezultat.sort((a, b) => sortSmjer==="asc" ?
+            a.bodovi-b.bodovi : b.bodovi-a.bodovi);
+    }
+
+    // pageSize = 5
+    // totalElements = 13
+    // totalPages =
+    const totalPages = Math.max(1, Math.ceil(rezultat.length/pageSize)); //2.3 -> 3
+
+    const start = (currentPage-1)*pageSize; //(2-1)*5 = 5
+    const end = start+pageSize;// 5 + 5 = 10
+    const igraciZaPrikaz = rezultat.slice(start, end);
+
+    if(currentPage>totalPages){
+        currentPage = totalPages;
+    }
+
     tableHtmlElement.innerHTML = `
         <thead>
           <tr>
@@ -25,29 +63,30 @@ function prikaziIgrace() {
               <div class="header-contents">
                 <span>Igrač</span>
                 <span class="sort-controls">
-                    <span class="sort-arrow" onclick="sortirajPoImenuAsc()">▲</span>
-                    <span class="sort-arrow" onclick="sortirajPoImenuDesc()">▼</span>
+                    <span class="sort-arrow" onclick="sortiraj('ime', 'asc')">▲</span>
+                    <span class="sort-arrow" onclick="sortiraj('ime', 'desc')">▼</span>
                 </span>
                </div>
-      
+              <input type="text" placeholder="Filtriraj po imenu..." value="${filterIme}" oninput="postaviFilterIme(this.value)">
             </th>
             <th>
               <div class="header-contents">
                 <span>Bodovi</span>
                 <span class="sort-controls">
-                    <span class="sort-arrow" onclick="sortirajPoBodovimaAsc()">▲</span>
-                    <span class="sort-arrow" onclick="sortirajPoBodovimaDesc()">▼</span>
+                    <span class="sort-arrow" onclick="sortiraj('bodovi', 'asc')">▲</span>
+                    <span class="sort-arrow" onclick="sortiraj('bodovi', 'desc')">▼</span>
                 </span>
                </div>
+               <input type="text" placeholder="Filtriraj po poenima..." value="${filterBodovi}" oninput="postaviFilterBodovi(this.value)">
             </th>
           </tr>
         </thead>    
-        <tbody id="tijelo">
-        </tbody>    
+        <tbody id="tijelo"></tbody>    
     `;
     const tBody = document.getElementById("tijelo");
-    igraci.forEach(igrac => {
-            tBody.innerHTML = tBody.innerHTML + `
+    let rows = "";
+    igraciZaPrikaz.forEach(igrac => {
+            rows = rows + `
                   <tr>
                     <td>${igrac.ime}</td>
                     <td>${igrac.bodovi}</td>
@@ -55,6 +94,13 @@ function prikaziIgrace() {
                 `;
         }
     );
+    if(igraciZaPrikaz.length === 0){
+        rows = `<tr><td colspan="2">Nema rezultata</td></tr>`
+    }
+    tBody.innerHTML = rows;
+
+    const pageInfoSpanElement = document.getElementById("page-info");
+    pageInfoSpanElement.textContent = `Stranica ${currentPage}/${totalPages}`;
 }
 
 function sortirajPoBodovimaAsc() {
@@ -67,14 +113,50 @@ function sortirajPoBodovimaDesc() {
     prikaziIgrace();
 }
 
-function sortirajPoImenuAsc() {
-    igraci.sort((igrac1, igrac2) => igrac2.ime.localeCompare(igrac1.ime));
+// function sortirajPoImenuAsc() {
+//     igraci.sort((igrac1, igrac2) => igrac2.ime.localeCompare(igrac1.ime));
+//     prikaziIgrace();
+// }
+//
+// function sortirajPoImenuDesc() {
+//     igraci.sort((igrac1, igrac2) => igrac1.ime.localeCompare(igrac2.ime));
+//     prikaziIgrace();
+// }
+
+function sortiraj(kolona, smjer){
+    sortKolona = kolona;
+    sortSmjer = smjer;
+    currentPage = 1;
     prikaziIgrace();
 }
 
-function sortirajPoImenuDesc() {
-    igraci.sort((igrac1, igrac2) => igrac1.ime.localeCompare(igrac2.ime));
+function postaviFilterIme(vrijednost){
+    filterIme = vrijednost;
+    currentPage = 1;
     prikaziIgrace();
+}
+
+function postaviFilterBodovi(vrijednost){
+    filterBodovi = vrijednost;
+    currentPage = 1;
+    prikaziIgrace();
+}
+
+function idiNaPrethodnuStranicu(){
+    if(currentPage>1){
+        currentPage--;
+        prikaziIgrace();
+    }
+}
+
+function idiNaSljedecuStranicu(){
+    let rezultat = [...igraci];
+    const totalPages = Math.max(1, Math.ceil(rezultat.length / pageSize));
+    if(currentPage <totalPages){
+        currentPage++;
+        prikaziIgrace();
+    }
+
 }
 
 prikaziIgrace();
